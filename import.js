3e6488150a8a34a14b2a0c330835c70f35145864
T!M Greller <timlg07@gmail.com>
2019-02-28 19:41:37 +0100

    added console-log when a file was successfully loaded

--- import.js
+++ import.js
@@ -19,19 +19,19 @@ function readFile( file ){
     
     let reader = new FileReader();
     reader.onload = function( evt ){
-        import_( evt.target.result );
+        import_( evt.target.result,file.name );
     };
     reader.readAsText(file);
     
 }
 
 // @param {String} The text of the text file with all values
-function import_( text ){
+function import_( text,name ){
     
     let new_cells = text.split( /[\r\n ]+/ ).filter(v=>v!="");
     while( $storage.speicherZellen.length < new_cells ){
         $storage.createTableRow();
     }
     $storage.speicher = new_cells.map(v=>(v==0?"":v));
-    
+    $console.log( `Imported ${encodeURI(name)}`,"#64DD17" );
 }
