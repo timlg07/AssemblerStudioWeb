14f4f451e495c2c03f63695677d811c9f5fdf08e
Tim Greller <timlg07@gmail.com>
2020-05-20 23:06:01 +0200

    Refactored code style of import.js

--- import.js
+++ import.js
@@ -1,37 +1,47 @@
-// (@param {Event} Gets passed by the listener)
+/**
+ * Handles the selection of a file. When the file is a valid plain text file,
+ * it gets imported.
+ * 
+ * @param {Event} evt The event gets passed by the listener
+ */ 
 function handleFileSelect( evt ){
-    
-    var file  = evt.target.files[0];
-    if( file ){
-        if( file.type.match( "text/plain" )){
-            readFile( file );
+    var file = evt.target.files[0];
+    if (file) {
+        if (file.type.match("text/plain")) {
+            readFile(file);
         } else {
-            $console.log( "The file has to be a plain text file.","red" );
+            $console.log("The file has to be a plain text file.", "red");
         }
     } else {
-        $console.log( "No file selected.","orange" );
+        $console.log("No file selected.", "orange");
     }
-    
-}    
-
-// @param {File} The file which should be loaded
-function readFile( file ){
-    
-    let reader = new FileReader();
-    reader.onload = function( evt ){
-        import_( evt.target.result,file.name );
-    };
-    reader.readAsText(file);
-    
 }
 
-// @param {String} The text of the text file with all values
-function import_( text,name ){
-    
-    let new_cells = text.split( /[\r\n ]+/ ).filter(v=>v!="");
-    while( $storage.speicherZellen.length < new_cells ){
+/**
+ * Loads the content of the file and passes it to the import function.
+ * 
+ * @param {File} file The file which should be loaded
+ */
+function readFile(file){
+    let reader = new FileReader();
+    reader.onload = function(evt) {
+        import_(evt.target.result, file.name);
+    };
+    reader.readAsText(file);
+}
+
+/** 
+ * Interprets the given text and overwrites the storage with its content.
+ * Writes a message to the $console when finished.
+ * 
+ * @param {String} text The text of the file containing all values.
+ * @param {String} name The filename used in the log message.
+ */
+function import_(text, name) {
+    let new_cells = text.split(/[\r\n ]+/).filter(v => v != "");
+    while ($storage.speicherZellen.length < new_cells) {
         $storage.createTableRow();
     }
-    $storage.speicher = new_cells.map(v=>(v==0?"":v));
-    $console.log( `Imported ${encodeURI(name)}`,"#64DD17" );
+    $storage.speicher = new_cells.map(v => v == 0 ? "" : v);
+    $console.log(`Imported ${encodeURI(name)}`, "#64DD17");
 }
